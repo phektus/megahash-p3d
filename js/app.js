700bbd4af67386344b0e65128943ccc1c809fbcb
Arbie Samong <phektus@gmail.com>
2014-01-06 04:10:05 +0800

    added localstorage sync override; added listener for file upload

--- js/app.js
+++ js/app.js
@@ -1,12 +1,24 @@
 $(function() {
-    var megahash = function(file, iterations) {
-	
-    }; // end megahash()
+    var getFileHash = function(file, iter, cb) {
+	var reader = new FileReader();
+	reader.onload = function() {
+	    var md5 = binl_md5(
+		reader.result,
+		reader.result.length
+	    );
+	    cb(null, md5);
+	}; // end onload
+	reader.onerror = function(err) {
+	    console.error('Could not read file:', err);
+	    cb(err, null);
+	}; // end onerror
+	reader.readAsBinaryString(file);
+    }; // end getFileHash()
 
     var Item = Backbone.Model.extend({
 	defaults: function() {
 	    return {
-		filename: null,
+		file: null,
 		hash: null,
 		iterations: 0
 	    };
@@ -15,30 +27,79 @@ $(function() {
 
     var ItemList = Backbone.Collection.extend({
 	model: Item,
-	
+	localStorage: new Backbone.LocalStorage("megahash")
     }); // end ItemList
     var Items = new ItemList;
+
+    var ItemView = Backbone.View.extend({
+	template: _.template(
+	    $('#item-template').html()
+	), // end template
+
+	initialize: function() {
+	    this.listenTo(
+		this.model,
+		'change',
+		this.render
+	    );
+	}, // end initialize
+
+	render: function() {
+	    this.$el.html(
+		this.template(
+		    this.model.toJSON()));
+	    return this;
+	}
+    }); // end ItemView
 
     var AppView = Backbone.View.extend({
 
 	el: $('#megahash-app'),
+
 	initialize: function() {
-	    console.log('Starting app');
+	    this.listenTo(
+		Items,
+		'add',
+		this.onAddItem
+	    );
 	},
+ 
 	events: {
 	    'click #add-item' : 'addItem',
+	    'change #choose-file': 'uploadFile',
 	    'click #remove-item' : 'removeItem',
 	    'click #cancel-hash' : 'cancelHash'
 	},
-	initialize: function() {
-	},
+
 	render: function() {
 	    console.log('Rendering view');
 	},
+
 	addItem: function(e) {
 	    console.log('Choosing file');
 	    $('#choose-file').click();
-	}
+	}, // end addItem
+
+	uploadFile: function(e) {
+	    var files = e.target.files;
+	    if(!files || files.length === 0) {
+		return;
+	    }
+	    Items.create({
+		file: files[0]
+	    });
+	}, // end uploadFile
+
+	onAddItem: function(item) {
+	    var view = new ItemView({ 
+		model:item
+	    });
+	    console.log('Added item on collection', view.render().el);
+	    this.$('#all-items').append(
+		view.render().el
+	    );
+	} // end onAddItem
+
     }); // end AppView
 
     var App = new AppView;
