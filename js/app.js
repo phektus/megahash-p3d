90e5afb8151a6c75b6b6e899e94a09e2bedaeeb6
Arbie Samong <phektus@gmail.com>
2014-01-06 14:09:44 +0800

    fixed file upload trigger timing

--- js/app.js
+++ js/app.js
@@ -1,19 +1,4 @@
 $(function() {
-    var getFileHash = function(file, iter, cb) {
-	var reader = new FileReader();
-	reader.onload = function() {
-	    var md5 = binl_md5(
-		reader.result,
-		reader.result.length
-	    );
-	    cb(null, md5);
-	}; // end onload
-	reader.onerror = function(err) {
-	    console.error('Could not read file:', err);
-	    cb(err, null);
-	}; // end onerror
-	reader.readAsBinaryString(file);
-    }; // end getFileHash()
 
     var Item = Backbone.Model.extend({
 	defaults: function() {
@@ -22,16 +7,56 @@ $(function() {
 		hash: null,
 		iterations: 0
 	    };
-	}
+	},
+
+	initialize: function() {
+	    this.on(
+		'change:file', 
+		this.generateHash
+	    );
+	},
+
+	generateHash: function() {
+	    var that = this;
+	    var file = that.get('file');
+	    var reader = new FileReader();
+
+	    console.log('Generating hash');
+	    if(!file) return;
+
+	    reader.onload = (function(myFile, that) {
+		return function(e) {
+		    that.set({
+			hash: binl_md5(
+			    reader.result,
+			    reader.result.length
+			)
+		    });
+		};
+	    })(file, this);
+
+	    reader.onerror = function(err) {
+		console.error('Could not read file:', err);
+	    }; // end onerror
+
+	    reader.readAsBinaryString(file);
+
+	} // end generateHash()
+
     }); // end Item
 
     var ItemList = Backbone.Collection.extend({
+
 	model: Item,
+
 	localStorage: new Backbone.LocalStorage("megahash")
+
     }); // end ItemList
+
     var Items = new ItemList;
 
     var ItemView = Backbone.View.extend({
+
 	template: _.template(
 	    $('#item-template').html()
 	), // end template
@@ -72,7 +97,7 @@ $(function() {
 	},
 
 	render: function() {
-	    console.log('Rendering view');
+	    
 	},
 
 	addItem: function(e) {
@@ -82,12 +107,14 @@ $(function() {
 
 	uploadFile: function(e) {
 	    var files = e.target.files;
+	    var file;
 	    if(!files || files.length === 0) {
 		return;
 	    }
-	    console.log(files[0]);
+	    file = files[0];
+	    console.log('Got file:', files);
 	    Items.create({
-		file: files[0]
+		file: file
 	    });
 	}, // end uploadFile
 
